1da686a6e1c0f89ef6a7e6b1235b095f9af49343
Muhammad Abdullah <ak8757796@gmail.com>
2026-08-07 21:42:02 +0500

    testing

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -1,12 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = "sqlite:///./blogagentapp.db"
+load_dotenv()
 
-engine = create_engine(URL_DATABASE, connect_args={"check_same_thread": False})
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL is not set")
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
 
 Base = declarative_base()
-
