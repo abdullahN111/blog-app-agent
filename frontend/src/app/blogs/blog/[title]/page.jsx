077306c75783fc1c363e20b3b7e00b0eee09d985
Muhammad Abdullah <ak8757796@gmail.com>
2026-09-03 15:44:01 +0500

    Improve Header and create blog page

--- frontend/src/app/blogs/blog/[title]/page.jsx
+++ frontend/src/app/blogs/blog/[title]/page.jsx
@@ -49,7 +49,7 @@ export default async function BlogPage({ params }) {
     <article className="min-h-screen bg-gray-50 py-8">
       <ViewTracker blogId={blog.id} />
       <div className="max-w-[1440px] mx-auto px-4 md:px-6">
-        <div className="max-w-5xl mx-auto">
+        <div className="max-w-[930px] mx-auto">
           <Link
             href="/blogs"
             className="inline-flex items-center text-middle hover:text-primary mb-4 transition-colors"
